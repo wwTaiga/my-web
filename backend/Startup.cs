808d5616e9aa971c15c1458688d1d2ddf9a73116
Taiga <qqtaiga@gmail.com>
2021-10-18 00:14:33 +0800

    backend: change namespace

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using MyWeb.DataContexts;
+using MyWeb.Data;
 using MyWeb.Repositories;
 using MyWeb.Settings;
 
