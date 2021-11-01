75b5a8b8b6559385adac5106fa002f9b9ce45663
Taiga <qqtaiga@gmail.com>
2021-11-02 02:39:32 +0800

    web: use LoginForm to connect backend api

--- frontend/src/components/account/LoginForm.tsx
+++ frontend/src/components/account/LoginForm.tsx
@@ -11,9 +11,45 @@ import {
     Heading,
     Text,
     useColorModeValue,
+    FormErrorMessage,
 } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
 
-export function LoginForm(): JSX.Element {
+interface Input {
+    username: string;
+    password: string;
+}
+
+const LoginForm = (): JSX.Element => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const doLogin = (input: Input) => {
+        const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(input),
+        };
+
+        fetch('http://localhost:5000/account/login', options)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw response;
+            })
+            .then((data) => {
+                if (data.status == 'success') {
+                    alert(data.data.token);
+                } else {
+                    alert(data.error.message);
+                }
+            });
+    };
+
     return (
         <Flex
             minH={'calc(100vh - 60px)'}
@@ -36,38 +72,59 @@ export function LoginForm(): JSX.Element {
                     p={8}
                 >
                     <Stack spacing={4}>
-                        <FormControl id="email">
-                            <FormLabel>Email address</FormLabel>
-                            <Input type="email" />
-                        </FormControl>
-                        <FormControl id="password">
-                            <FormLabel>Password</FormLabel>
-                            <Input type="password" />
-                        </FormControl>
-                        <Stack spacing={10}>
-                            <Stack
-                                direction={{ base: 'column', sm: 'row' }}
-                                align={'start'}
-                                justify={'space-between'}
-                            >
-                                <Checkbox>Remember me</Checkbox>
-                                <Link color={'blue.400'}>Forgot password?</Link>
+                        <form onSubmit={handleSubmit(doLogin)}>
+                            <FormControl isInvalid={errors.userName}>
+                                <FormLabel>User Name</FormLabel>
+                                <Input
+                                    type="text"
+                                    {...register('userName', {
+                                        required: 'This is required',
+                                    })}
+                                />
+                                <FormErrorMessage>
+                                    {errors.userName && errors.userName.message}
+                                </FormErrorMessage>
+                            </FormControl>
+                            <FormControl isInvalid={errors.password}>
+                                <FormLabel>Password</FormLabel>
+                                <Input
+                                    type="password"
+                                    {...register('password', {
+                                        required: 'This is required',
+                                    })}
+                                />
+                                <FormErrorMessage>
+                                    {errors.password && errors.password.message}
+                                </FormErrorMessage>
+                            </FormControl>
+                            <Stack spacing={10}>
+                                <Stack
+                                    direction={{ base: 'column', sm: 'row' }}
+                                    align={'start'}
+                                    justify={'space-between'}
+                                >
+                                    <Checkbox>Remember me</Checkbox>
+                                    <Link color={'blue.400'}>
+                                        Forgot password?
+                                    </Link>
+                                </Stack>
+                                <Button
+                                    type="submit"
+                                    bg={'blue.400'}
+                                    color={'white'}
+                                    _hover={{
+                                        bg: 'blue.500',
+                                    }}
+                                >
+                                    Sign in
+                                </Button>
                             </Stack>
-                            <Button
-                                bg={'blue.400'}
-                                color={'white'}
-                                _hover={{
-                                    bg: 'blue.500',
-                                }}
-                            >
-                                Sign in
-                            </Button>
-                        </Stack>
+                        </form>
                     </Stack>
                 </Box>
             </Stack>
         </Flex>
     );
-}
+};
 
 export default LoginForm;
