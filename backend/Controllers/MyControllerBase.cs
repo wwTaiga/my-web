f8466844bea091be5250ef0f451ed2d701819832
Taiga <qqtaiga@gmail.com>
2021-12-30 15:57:07 +0800

    backend: fix unable to use swagger ui

--- backend/Controllers/MyControllerBase.cs
+++ backend/Controllers/MyControllerBase.cs
@@ -7,9 +7,14 @@ namespace MyWeb.Controllers
     public abstract class MyControllerBase : ControllerBase
     {
         /// <summary>
-        /// 
+        /// Convert <c>IdentityResult</c> to a json object and produce 422 response. 
         /// </summary>
-        public ActionResult UnprocessableEntity(IdentityResult result)
+        /// <param name="result">Identity result</param>
+        /// <returns>
+        /// UnprocessableEntity (422) response with error description.
+        /// </returns>
+        [NonAction]
+        public UnprocessableEntityObjectResult UnprocessableEntity(IdentityResult result)
         {
             Dictionary<string, string> errorDescs = new();
             foreach (IdentityError error in result.Errors)
