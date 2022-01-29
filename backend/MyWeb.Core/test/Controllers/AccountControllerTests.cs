3468ddc8eed3e1fc6fe539ec53262ea95f17bcc4
Taiga <qqtaiga@gmail.com>
2022-01-30 02:21:44 +0800

    backend: rename folder name

---
+++ backend/MyWeb.Core/test/Controllers/AccountControllerTests.cs
@@ -0,0 +1,21 @@
+using MyWeb.Core.Controllers;
+using Xunit;
+
+namespace MyWeb.Core.Tests.Controllers
+{
+    public class AccountControllerTests
+    {
+        private AccountController _controller;
+
+        public AccountControllerTests()
+        {
+            _controller = new AccountController(null, null, null, null, null, null);
+        }
+
+        [Fact]
+        public void DoRegister()
+        {
+            var tets = new AccountController(null, null, null, null, null, null);
+        }
+    }
+}
