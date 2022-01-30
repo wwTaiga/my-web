86277c4e094c22483e012bd681e535430faf388b
Taiga <qqtaiga@gmail.com>
2022-01-31 01:04:44 +0800

    backend: add some test case for Account Controller

--- backend/MyWeb.Core/test/Controllers/AccountControllerTests.cs
+++ backend/MyWeb.Core/test/Controllers/AccountControllerTests.cs
@@ -1,21 +1,62 @@
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 using MyWeb.Core.Controllers;
+using MyWeb.Core.Models.Dtos;
+using MyWeb.Core.Models.Entities;
 using Xunit;
 
 namespace MyWeb.Core.Tests.Controllers
 {
     public class AccountControllerTests
     {
-        private AccountController _controller;
-
-        public AccountControllerTests()
+        [Fact]
+        public async Task DoRegister_NewUser_Return200()
         {
-            _controller = new AccountController(null, null, null, null, null, null);
+            // Arrange
+            Mock<UserManager<LoginUser>> mockUserManager = new(Mock.Of<IUserStore<LoginUser>>(),
+                    null, null, null, null, null, null, null, null);
+            mockUserManager.Setup(e => e.CreateAsync(It.IsAny<LoginUser>(), It.IsAny<string>()))
+                .ReturnsAsync(IdentityResult.Success);
+            AccountController controller = new(null, null, null, mockUserManager.Object, null, null);
+            RegisterDto registerDto = new("test", "test", "some@email.com");
+
+            // Act
+            var result = await controller.DoRegister(registerDto);
+
+            // Assert
+            var okResult = result as OkResult;
+            okResult.Should().NotBeNull();
+            if (okResult != null)
+                okResult.StatusCode.Should().Be(200);
         }
 
         [Fact]
-        public void DoRegister()
+        public async Task DoRegister_DuplicateUser_Return422()
         {
-            var tets = new AccountController(null, null, null, null, null, null);
+            // Arrange
+            Mock<UserManager<LoginUser>> mockUserManager = new(Mock.Of<IUserStore<LoginUser>>(),
+                    null, null, null, null, null, null, null, null);
+            mockUserManager.Setup(e => e.CreateAsync(It.IsAny<LoginUser>(), It.IsAny<string>()))
+                .ReturnsAsync(IdentityResult.Failed(
+                    new IdentityError() { Code = "Duplicate", Description = "Duplicate user." }));
+            AccountController controller = new(null, null, null, mockUserManager.Object, null, null);
+            controller.ControllerContext = new ControllerContext();
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            controller.ControllerContext.HttpContext.TraceIdentifier = "12345";
+            RegisterDto registerDto = new("test", "test", "some@email.com");
+
+            // Act
+            var result = await controller.DoRegister(registerDto);
+
+            // Assert
+            var unprocessableResult = result as UnprocessableEntityObjectResult;
+            unprocessableResult.Should().NotBeNull();
+            if (unprocessableResult != null)
+                unprocessableResult.StatusCode.Should().Be(422);
         }
     }
 }
